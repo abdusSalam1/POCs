f22209e409c6447520024647134688502167355c
abdusalam <abdus.salam@cliclchain.com>
2016-10-19 02:58:42 -0700

    User service bearn

--- VaadinExample/app/src/main/java/com/ui/AppUI.java
+++ VaadinExample/app/src/main/java/com/ui/AppUI.java
@@ -1,10 +1,12 @@
 package com.ui;
 
+import com.service.UserService;
 import com.vaadin.annotations.PreserveOnRefresh;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by Abdus Salam on 9/12/2016.
@@ -13,8 +15,12 @@ import com.vaadin.ui.VerticalLayout;
 @PreserveOnRefresh
 public class AppUI extends UI {
 
+    @Autowired
+    public UserService userService;
+
     @Override
     protected void init(VaadinRequest request) {
+        userService.getUser();
         VerticalLayout layout = new VerticalLayout();
         layout.addComponent(new Label("hi"));
         setContent(layout);
